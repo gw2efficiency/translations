6ce968ae53daacb73e404046ba30ece46a84a25a
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2019-11-22 11:50:45 +0100

    Adds myself in the credits

--- credits.js
+++ credits.js
@@ -4,6 +4,7 @@
 const current = [
   {name: 'Ainillia.5794', language: 'French'},
   {name: 'EDCDragon.8356', language: 'French'},
+  {name: 'Pilou.3498', language: 'French'},
   {name: 'jonesarte.2980', language: 'Spanish'},
   {name: 'MrSmiley.9273', language: 'German'},
   {name: 'Regitus.7189', language: 'German'}
